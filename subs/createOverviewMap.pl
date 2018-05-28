5d24979f9cc32b314890bc4eeaf44fb18198b220
flo.maurer <flo.maurer@tum.de>
2018-05-28 14:01:58 -0700

    initial Idea of Overview Map - new files not included in main prog yet

---
+++ subs/createOverviewMap.pl
@@ -0,0 +1,12 @@
+sub createOverviewMap {
+    # get StartpointCoordinates + date + name
+
+    # partitionate stratpoints in groups for usefull map zoom
+
+    # download + plot maps
+
+    # add starticons with refs ('\label{',$Activity_date,'-',$Goal,'}') to tour
+
+
+}
+1;
