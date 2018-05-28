c0e95672ade12aafbd7af73bea3f93232816147a
flo.maurer <flo.maurer@tum.de>
2018-05-28 15:16:58 -0700

    Added linux start script

--- _do_only.bat
+++
@@ -1,5 +0,0 @@
-REM call .\tracks\_FIT2GPX.bat
-REM for /r %%v in (.\tracks\2018*.FIT) do perl ./code/FIT2GPX/fit2gpx.pl "%%v"
-REM perltex --nosafe --latex=pdflatex "Tourenverzeichnis-2018".tex
-perltex --nosafe --latex=pdflatex "Tourenverzeichnis-2018".tex
-Tourenverzeichnis-2018.pdf
